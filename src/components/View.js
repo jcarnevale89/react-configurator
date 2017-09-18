2f17604c829a11652c484e19440c6df96cee45ff
Jake Carnevale <jake@autocustoms.com>
2017-09-18 17:13:53 -0500

    Add fading to colors using CSS animations. Once the new color is loaded it triggers the old color to fadeout

--- src/components/View.js
+++ src/components/View.js
@@ -3,10 +3,31 @@ import Part from './Part'
 
 const View = (props) => {
 
+  const hideColor = (e) => {
+    if (e.target.classList[0] !== 'newColor') return
+    e.target.nextSibling.classList.add('active')
+  }
+
+  const colors = [...props.currentColors].reverse()
+
   return (
     <div className={`view view-${props.view}`}>
+      <div className="loader">loader</div>
       <img src={`images/${props.truckId}/${props.truckId}-${props.view}.jpg`} alt={`${props.truckId}-${props.view}`}/>
-      <img src={`images/${props.truckId}/${props.truckId}-${props.color}-${props.view}.png`} alt={`${props.truckId}-${props.view}`}/>
+      {
+        colors.map((color, i) => {
+          let classes = 'newColor'
+          if (i === 1) {
+            classes = 'oldColor'
+          }
+          if (props.currentColors.length < 2) {
+            classes = 'first'
+          }
+          return (
+            <img key={`${color}-${props.view}`} src={`images/${props.truckId}/${props.truckId}-${color}-${props.view}.png`} className={classes} onLoad={hideColor} alt={`${props.truckId}-${props.view}`}/>
+          )
+        })
+      }
       {
         props.parts.map((part) => {
           return (
@@ -14,7 +35,7 @@ const View = (props) => {
               key={part.code}
               truckId={props.truckId}
               view={props.view}
-              color={props.color}
+              color={props.currentColor}
               part={part.code}
             />
           )
